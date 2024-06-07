703da9c4c2df41f567f5124df08c7125777fab91
Adithya Mallya <adimally@publicisgroupe.net>
2024-06-08 00:12:57 +0530

    ADD: Interceptor for custom responses

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToInstance } from 'class-transformer';
+
+//Customm decorator
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+//Customer Interceptor to seriaze the response object using Dto
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    // Do something before request is sent to handler
+    return next.handle().pipe(
+      map((data: any) => {
+        // Do something before sending out the response
+
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
