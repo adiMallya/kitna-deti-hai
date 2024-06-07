4aa7f0a82bdc532d17655e8770dbe1d706fe9a5c
Adithya Mallya <adimally@publicisgroupe.net>
2024-06-07 18:24:41 +0530

    ADD: Routes for create, find, findOne, remove, update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  //Repository injection
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException(`User by the id ${id} not found.`);
+    }
+
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  remove(id: number) {
+    return this.repo.delete({ id });
+  }
+}
