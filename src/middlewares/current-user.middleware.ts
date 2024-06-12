244eb5a27886a9ef87a91e090008195259653e1d
Adithya Mallya <adimally@publicisgroupe.net>
2024-06-12 10:50:03 +0530

    ADD: Current user as middleware

---
+++ src/middlewares/current-user.middleware.ts
@@ -0,0 +1,27 @@
+import { NestMiddleware, Injectable } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { User } from 'src/users/user.entity';
+import { UsersService } from 'src/users/users.service';
+
+// To add currUser prop to default Request interface of express library
+declare global{
+  namespace Express{
+    interface Request{
+      currUser?: User;
+    }
+  }
+}
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware {
+  constructor(private usersService: UsersService) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    const { userId } = req.session ?? {};
+
+    if (userId) {
+      const user = await this.usersService.findOne(userId);
+      req.currUser = user;
+      next();
+    }
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,18 +1,27 @@
-import { Module } from '@nestjs/common';
-import {APP_INTERCEPTOR} from '@nestjs/core';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './user.entity';
 import { AuthService } from './auth.service';
 import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { CurrentUserMiddleware } from 'src/middlewares/current-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService, AuthService, {
-    provide: APP_INTERCEPTOR,
-    useClass: CurrentUserInterceptor
-  }],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    },
+  ],
   controllers: [UsersController],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
