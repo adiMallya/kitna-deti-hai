70b7fd2ff33bd26a78b69b3fd2d2b0ea0254fc6f
Adithya Mallya <adimally@publicisgroupe.net>
2024-06-14 00:39:52 +0530

    FIX: Injected depedencies

--- src/reports/reports.service.spec.ts
+++ src/reports/reports.service.spec.ts
@@ -1,15 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReportsService } from './reports.service';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('ReportsService', () => {
   let service: ReportsService;
+  let repository: Repository<Report>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReportsService],
+      providers: [ReportsService, {
+        provide: getRepositoryToken(Report),
+        useClass: Repository
+      }],
     }).compile();
 
     service = module.get<ReportsService>(ReportsService);
+    repository = module.get(getRepositoryToken(Report));
   });
 
   it('should be defined', () => {
