e7d66075774ca9e37170af7d72fca9a80b906b7a
Adithya Mallya <adimally@publicisgroupe.net>
2024-06-13 12:29:06 +0530

    REFACTOR: Added estimation logic, dto

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Post, Body, Get, Patch, UseGuards, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Patch, UseGuards, Param, Query, Put } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos/create-report.dto';
-import { AuthGuard } from 'src/guards/auth.guard';
-import { CurrentUser } from 'src/users/decorators/current-user.decorator';
-import { User } from 'src/users/user.entity';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
-import { AdminGuard } from 'src/guards/admin.guard';
+import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -24,5 +25,10 @@ export class ReportsController {
   @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
+  }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.createQuery(query);
   }
 }
